ce1470ef3aea057f92045af30d94f944a081eec3
GregoryGost <huzver@gmail.com>
2024-05-12 18:05:33 +0300

    fix Linting SHELL_SHFMT

--- scripts/update.sh
+++ scripts/update.sh
@@ -3,15 +3,15 @@
 BRANCH="$1"
 
 if [[ -n "$BRANCH" ]]; then
-BRANCH="master"
+	BRANCH="master"
 fi
 
 if which git 2> /dev/null > /dev/null; then
-echo "Updating source..."
-git pull origin "$BRANCH"
+	echo "Updating source..."
+	git pull origin "$BRANCH"
 else
-echo "Skipped Updating sources, no GIT found"
-exit 0
+	echo "Skipped Updating sources, no GIT found"
+	exit 0
 fi
 
 echo "Update sources successful!"
